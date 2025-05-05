f3a3b1e4a926d376599ca028f5be0419394b95a5
Tzeo <134011898+Geoulis23@users.noreply.github.com>
2025-05-05 19:00:14 +0300

    Update App.java

--- JavaFXProject/src/main/java/project/unipi/gui/JavaFXProject/App.java
+++ JavaFXProject/src/main/java/project/unipi/gui/JavaFXProject/App.java
@@ -35,9 +35,9 @@ public class App extends Application {
     	
     	SceneCreator bookManagementSceneCreator = new BookManagementSceneCreator(850, 350);
     	bookManagementScene = bookManagementSceneCreator.createScene();
+    	
     	SceneCreator studentManagementSceneCreator = new StudentManagementSceneCreator(900, 350);
     	studentManagementScene = studentManagementSceneCreator.createScene();
-//    	studentManagementScene = new Scene(new GridPane(), 650, 300);
 //    	loanManagementScene = new Scene(new GridPane(), 650, 300);
 //    	paymentManagementScene = new Scene(new GridPane(), 650, 300);
     	
@@ -47,19 +47,6 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-    	//Starting Books List
-        List<Book> books = new ArrayList<>();  
-        //BOOKS
-        books.add(new Book("978-960-453-709-4", "Harry Potter and the Chamber of Secrets", "J.K Rowling", "Scholastic Inc", 1999, "Fantasy", true));
-        books.add(new Book("233-890-213-234-8", "The Hunger Games", "Suzanne Collins", "Scholastic Inc", 2008, "Action/Drama", false));
-        books.add(new Book("358-666-437-895-3", "The Little Prince", "Antoine de Saint-Exupéry", "Reynal & Hitchcock", 1943, "Fantasy", true));
-        books.add(new Book("577-230-433-125-4", "The Lord of the rings", "J.R.R. Tolkien", "Houghton Mifflin Harcourt", 2001, "Fantasy", true));
-        books.add(new Book("344-450-853-009-1", "Don Quixote", "Miguel de Cervantes", "Francisco de Robles", 1605, "Tragedy", false));
-        books.add(new Book("245-326-783-769-6", "Pride and Prejudice", "Jane Austen", "T. Egerton, Whitehall", 1813, "Romantic", true));
-        books.add(new Book("921-320-124-421-8", "War And Peace", "Leo Tolstoy", "Oxford University Press", 1867, "Historical", true));
-        books.add(new Book("654-230-218-746-0", "Great Expectations", "Charles Dickens", "Simon & Schuster", 1861, "Mystery", false));
-        books.add(new Book("912-135-473-127-8", "To Kill A Mockingbird", "Harper Lee", "HarperCollins", 1960, "Gothic", true));
-        books.add(new Book("125-789-233-726-2", "Moby-Dick", "Herman Melville", "Richard Bentley (United Kingdom) Harper & Brothers (U.S.)", 1851, "Adventure", true));
         //Starting Students List
         List<Student> students = new ArrayList<>();    
         //STUDENTS 
