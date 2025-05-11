5014d793c5623fbd332bc7cf4c09198fbb90a7f1
Tzeo <134011898+Geoulis23@users.noreply.github.com>
2025-05-11 17:36:03 +0300

    Update BookManagementSceneCreator.java

--- JavaFXProject/src/main/java/project/unipi/gui/JavaFXProject/BookManagementSceneCreator.java
+++ JavaFXProject/src/main/java/project/unipi/gui/JavaFXProject/BookManagementSceneCreator.java
@@ -2,6 +2,7 @@ package project.unipi.gui.JavaFXProject;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
@@ -9,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
@@ -25,7 +27,7 @@ public class BookManagementSceneCreator extends SceneCreator implements EventHan
 	//Grid Pane
 	GridPane rootGridPane, inputGridPane;
 	//Buttons
-	Button newBtn, updateBtn, delBtn, backBtn;
+	Button newBtn, updateBtn, delBtn, backBtn, clearBtn;
 	//Labels
 	Label ISBNLbl, titleLbl, authorLbl, publisherLbl, yopLbl, catLbl, availLbl;
 	//Text Fields
@@ -35,9 +37,12 @@ public class BookManagementSceneCreator extends SceneCreator implements EventHan
 	//Table View
 	TableView<Book> bookTableView;
 	//Book list
-	List<Book> books;
+	ArrayList<Book> books;
 	//Alert
 	Alert errorAlert = new Alert(AlertType.ERROR);
+	Alert confAlert = new Alert(AlertType.CONFIRMATION);
+	//ButtonTypes
+	ButtonType buttonYes, buttonNo;
 	
 	public BookManagementSceneCreator(double width, double height) {
 		super(width, height);
@@ -50,13 +55,18 @@ public class BookManagementSceneCreator extends SceneCreator implements EventHan
 		updateBtn = new Button("Edit Book Fields");
 		delBtn = new Button("Delete Book");
 		backBtn = new Button("Back");
+		clearBtn = new Button("Clear");
 		//Connect Buttons with handler
 		newBtn.setOnMouseClicked(this);
 		updateBtn.setOnMouseClicked(this);
 		delBtn.setOnMouseClicked(this);
 		backBtn.setOnMouseClicked(this);
-		
-		//bookTableView.setOnMouseClicked(this);
+		clearBtn.setOnMouseClicked(this);
+		//ButtonType initialization
+		buttonYes = new ButtonType("Yes");
+		buttonNo = new ButtonType("No");
+		//Connect ButtonTypes with alert
+		confAlert.getButtonTypes().setAll(buttonYes, buttonNo);
 		//Label Initialization
 		ISBNLbl = new Label("ISBN: ");
 		titleLbl = new Label("Title: ");
@@ -80,7 +90,9 @@ public class BookManagementSceneCreator extends SceneCreator implements EventHan
 		//Alert Customization
 		errorAlert.setTitle("Error Occurred");
 		errorAlert.setHeaderText(null);
-		//ISBNAlert.setContentText("ISBN is empty! Please type in an ISBN in order to edit a book.");
+		confAlert.setTitle("Confirmation Alert");
+//		confAlert.setHeaderText("The input you gave contains numbers");
+		confAlert.setContentText("You sure you want to continue?");
 		//Customize Flowpane
 		btnFlowPane.setHgap(15);
 		btnFlowPane.getChildren().add(newBtn);
@@ -105,6 +117,7 @@ public class BookManagementSceneCreator extends SceneCreator implements EventHan
 		inputGridPane.add(catField, 1, 5);
 		inputGridPane.add(availLbl, 0, 6);
 		inputGridPane.add(availCheckBox, 1, 6);
+		inputGridPane.add(clearBtn, 1, 8);
 		//Customize Root GridPane
 		rootGridPane.setVgap(10);
 		rootGridPane.setHgap(10);
@@ -146,16 +159,16 @@ public class BookManagementSceneCreator extends SceneCreator implements EventHan
 		//Initialize the ArrayList for books
 		books = new ArrayList<>();
 		books.add(new Book("978-960-453-709-4", "Harry Potter and the Chamber of Secrets", "J.K Rowling", "Scholastic Inc", 1999, "Fantasy", true));
-        	books.add(new Book("233-890-213-234-8", "The Hunger Games", "Suzanne Collins", "Scholastic Inc", 2008, "Action/Drama", false));
-        	books.add(new Book("358-666-437-895-3", "The Little Prince", "Antoine de Saint-Exupéry", "Reynal & Hitchcock", 1943, "Fantasy", true));
-        	books.add(new Book("577-230-433-125-4", "The Lord of the rings", "J.R.R. Tolkien", "Houghton Mifflin Harcourt", 2001, "Fantasy", true));
-        	books.add(new Book("344-450-853-009-1", "Don Quixote", "Miguel de Cervantes", "Francisco de Robles", 1605, "Tragedy", false));
-        	books.add(new Book("245-326-783-769-6", "Pride and Prejudice", "Jane Austen", "T. Egerton, Whitehall", 1813, "Romantic", true));
-        	books.add(new Book("921-320-124-421-8", "War And Peace", "Leo Tolstoy", "Oxford University Press", 1867, "Historical", true));
-        	books.add(new Book("654-230-218-746-0", "Great Expectations", "Charles Dickens", "Simon & Schuster", 1861, "Mystery", false));
-        	books.add(new Book("912-135-473-127-8", "To Kill A Mockingbird", "Harper Lee", "HarperCollins", 1960, "Gothic", true));
-       		books.add(new Book("125-789-233-726-2", "Moby-Dick", "Herman Melville", "Richard Bentley (United Kingdom) Harper & Brothers (U.S.)", 1851, "Adventure", true));
-        	tableSync();
+        books.add(new Book("233-890-213-234-8", "The Hunger Games", "Suzanne Collins", "Scholastic Inc", 2008, "Action/Drama", false));
+        books.add(new Book("358-666-437-895-3", "The Little Prince", "Antoine de Saint-Exupéry", "Reynal & Hitchcock", 1943, "Fantasy", true));
+        books.add(new Book("577-230-433-125-4", "The Lord of the rings", "J.R.R. Tolkien", "Houghton Mifflin Harcourt", 2001, "Fantasy", true));
+        books.add(new Book("344-450-853-009-1", "Don Quixote", "Miguel de Cervantes", "Francisco de Robles", 1605, "Tragedy", false));
+        books.add(new Book("245-326-783-769-6", "Pride and Prejudice", "Jane Austen", "T. Egerton, Whitehall", 1813, "Romantic", true));
+        books.add(new Book("921-320-124-421-8", "War And Peace", "Leo Tolstoy", "Oxford University Press", 1867, "Historical", true));
+        books.add(new Book("654-230-218-746-0", "Great Expectations", "Charles Dickens", "Simon & Schuster", 1861, "Mystery", false));
+        books.add(new Book("912-135-473-127-8", "To Kill A Mockingbird", "Harper Lee", "HarperCollins", 1960, "Gothic", true));
+        books.add(new Book("125-789-233-726-2", "Moby-Dick", "Herman Melville", "Richard Bentley (United Kingdom) Harper & Brothers (U.S.)", 1851, "Adventure", true));
+        tableSync();
 	}
 
 	@Override
@@ -164,53 +177,206 @@ public class BookManagementSceneCreator extends SceneCreator implements EventHan
 			App.stage.setTitle("UniPi Library");
 			App.stage.setScene(App.mainScene);
 		}
-		if(event.getSource() == newBtn) {
-			int yop = 0;
-			
-			//Get the input from the textfields
-			String ISBN = ISBNField.getText();
-			String title = titleField.getText();
-			String author = authorField.getText();
-			String publisher = publisherField.getText();
-			String cat = catField.getText();
-			boolean avail = availCheckBox.isSelected();
-			try {
-				yop = Integer.parseInt(yopField.getText());
-			}catch(NumberFormatException e){
-				errorAlert.setContentText("Invalid year format. Making it 0 by default you can edit and change it.");
-				errorAlert.show();
-			}
-			
-			//Create the new book
-			createBook(ISBN, title, author, publisher, yop, cat, avail);
-			
-			//synchronize the list
-			tableSync();
+		if(event.getSource() == clearBtn) {
 			clearTextFields();
 		}
-		if(event.getSource() == updateBtn) {
-			//Check if there is a blank field
+		if(event.getSource() == newBtn) { // to isbn na einai mono ari8moi, author k publisher mono grammata, otan den exoun input ta pedia na mhn ftiaxnei biblio
 			if(ISBNField.getText().equals("") || titleField.getText().equals("") || authorField.getText().equals("") || publisherField.getText().equals("") || yopField.getText().equals("") || catField.getText().equals("")) {
-				errorAlert.setContentText("Field(s) blank. Make sure you've slected a book and made the changes you want.");
+				errorAlert.setContentText("Field(s) blank. Make sure you've filled out everything");
 				errorAlert.show();
 			}
 			else {
-				
 				int yop = 0;
-				//Get the input from the textfields
-				String ISBN = ISBNField.getText();
-				String title = titleField.getText();
-				String author = authorField.getText();
-				String publisher = publisherField.getText();
-				String cat = catField.getText();
+				String ISBN = "", title = "", author = "" , publisher = "", cat = "";
+				
+				//GET THE INPUT FROM TEXTFIELDS
+				//ISBN Check
+				if(check(ISBNField.getText()) == true) {
+					ISBN = ISBNField.getText();
+				}
+				else {
+					errorAlert.setContentText("Invalid ISBN format.");
+					errorAlert.show();
+					ISBNField.setText("");
+				}
+				
+				title = titleField.getText();
+				
+				//Author Check
+				if(check(authorField.getText()) == true) {
+					confAlert.setHeaderText("The author input you gave contains numbers");
+					Optional<ButtonType> result = confAlert.showAndWait();
+					
+					if (result.isPresent() && result.get() == buttonYes) {
+				        System.out.println("Yes");
+				        author = authorField.getText();
+				    } 
+					else {
+				        System.out.println("No");
+				        authorField.setText("");
+				    }
+				}
+				else {
+					author = authorField.getText();
+				}
+				
+				//Publisher Check
+				if(check(publisherField.getText()) == true) {
+					confAlert.setHeaderText("The publisher input you gave contains numbers");
+					Optional<ButtonType> result = confAlert.showAndWait();
+					
+					if (result.isPresent() && result.get() == buttonYes) {
+				        System.out.println("Yes");
+				        publisher = publisherField.getText();
+				    } 
+					else {
+				        System.out.println("No");
+				        publisherField.setText("");
+				    }
+				}
+				else {
+					publisher = publisherField.getText();
+				}
+				
+				//CAT Check
+				if(check(catField.getText()) == true) {
+					confAlert.setHeaderText("The category input you gave contains numbers");
+					Optional<ButtonType> result = confAlert.showAndWait();
+					
+					if (result.isPresent() && result.get() == buttonYes) {
+				        System.out.println("Yes");
+				        cat = catField.getText();
+				    } 
+					else {
+				        System.out.println("No");
+				        catField.setText("");
+				    }
+				}
+				else {
+					cat = catField.getText();
+				}
+				
 				boolean avail = availCheckBox.isSelected();
+				
+				//Year Of Publish check
 				try {
 					yop = Integer.parseInt(yopField.getText());
 				}catch(NumberFormatException e){
 					errorAlert.setContentText("Invalid year format. Making it 0 by default you can edit and change it.");
 					errorAlert.show();
+					yopField.setText("");
 				}
 				
+				if(!(ISBNField.getText().equals("") || titleField.getText().equals("") || authorField.getText().equals("") || publisherField.getText().equals("") || yopField.getText().equals("") || catField.getText().equals(""))) {
+					//Create the new book
+					createBook(ISBN, title, author, publisher, yop, cat, avail);
+					
+					//synchronize the list
+					tableSync();
+					clearTextFields();
+				}
+			}
+		}
+		if(event.getSource() == updateBtn) {
+			//Check if there is a blank field
+//			if(ISBNField.getText().equals("") || titleField.getText().equals("") || authorField.getText().equals("") || publisherField.getText().equals("") || yopField.getText().equals("") || catField.getText().equals("")) {
+//				errorAlert.setContentText("Field(s) blank. Make sure you've slected a book and made the changes you want.");
+//				errorAlert.show();
+//			}
+			if(ISBNField.getText().equals("")) {
+				errorAlert.setContentText("ISBN is mandatory in order to edit a book!");
+				errorAlert.show();
+			}
+			else {
+				int yop = 0;
+				String ISBN = "", title = "", author = "" , publisher = "", cat = "";
+				
+				//GET THE INPUT FROM TEXTFIELDS
+				//ISBN Check
+				if(check(ISBNField.getText()) == true) {
+					ISBN = ISBNField.getText();
+				}
+				else {
+					errorAlert.setContentText("Invalid ISBN format.");
+					errorAlert.show();
+					ISBNField.setText("");
+					//ISBNField.setText("");
+				}
+				
+				title = titleField.getText();
+				
+				//Author Check
+				if(check(authorField.getText()) == true) {
+					confAlert.setHeaderText("The author input you gave contains numbers");
+					Optional<ButtonType> result = confAlert.showAndWait();
+					
+					if (result.isPresent() && result.get() == buttonYes) {
+				        System.out.println("Yes");
+				        author = authorField.getText();
+				    } 
+					else {
+				        System.out.println("No");
+				        authorField.setText("");
+				    }
+				}
+				else {
+					author = authorField.getText();
+				}
+				
+				//Publisher Check
+				if(check(publisherField.getText()) == true) {
+					confAlert.setHeaderText("The publisher input you gave contains numbers");
+					Optional<ButtonType> result = confAlert.showAndWait();
+					
+					if (result.isPresent() && result.get() == buttonYes) {
+				        System.out.println("Yes");
+				        publisher = publisherField.getText();
+				    } 
+					else {
+				        System.out.println("No");
+				        publisherField.setText("");
+				    }
+				}
+				else {
+					publisher = publisherField.getText();
+				}
+				
+				//CAT Check
+				if(check(catField.getText()) == true) {
+					confAlert.setHeaderText("The category input you gave contains numbers");
+					Optional<ButtonType> result = confAlert.showAndWait();
+					
+					if (result.isPresent() && result.get() == buttonYes) {
+				        System.out.println("Yes");
+				        cat = catField.getText();
+				    } 
+					else {
+				        System.out.println("No");
+				        catField.setText("");
+				    }
+				}
+				else {
+					cat = catField.getText();
+				}
+				
+				boolean avail = availCheckBox.isSelected();
+				
+				try {
+					yop = Integer.parseInt(yopField.getText());
+				}catch(NumberFormatException e){
+					errorAlert.setContentText("Invalid year format. Making it 0 by default you can edit and change it.");
+					errorAlert.show();
+					yopField.setText("");
+				}
+				
+//				if(!(ISBNField.getText().equals("") || titleField.getText().equals("") || authorField.getText().equals("") || publisherField.getText().equals("") || yopField.getText().equals("") || catField.getText().equals(""))) {
+//					//Update the selected book
+//					updateBook(ISBN, title, author, publisher, yop, cat, avail);
+//					
+//					//synchronize the list
+//					tableSync();
+//					clearTextFields();
+//				}
 				//Update the selected book
 				updateBook(ISBN, title, author, publisher, yop, cat, avail);
 				
@@ -248,7 +414,7 @@ public class BookManagementSceneCreator extends SceneCreator implements EventHan
 		books.add(b);
 	}
 
-	private void updateBook(String _ISBN, String _title, String _author, String _publisher, int _yop, String _cat, boolean _avail) {
+	public void updateBook(String _ISBN, String _title, String _author, String _publisher, int _yop, String _cat, boolean _avail) {
 		for(Book b: books) {
 			if(b.getISBN().equals(_ISBN)) {
 				b.setTitle(_title);
@@ -289,4 +455,22 @@ public class BookManagementSceneCreator extends SceneCreator implements EventHan
 		catField.setText("");
 		availCheckBox.setSelected(false);
 	}
+	
+	boolean check(String input) {
+		//int plN = 0, plD = 0, plC = 0;
+		
+		for (char c : input.toCharArray()) {
+		      if (!Character.isDigit(c)) {
+		    	  if(c == '-') {
+		    		 continue;
+		    	  }
+		    	  else {
+		    		  return false;
+		    	  }
+		      } 
+		}
+		return true;
+	}
+	
+	
 }
